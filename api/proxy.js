1c1edca6435ef6667e3e28c0e6754aa6c259f862
pwxmarco <t3chzoneyt@gmail.com>
2026-03-12 11:26:11 +0530

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+
+const target = "https://pwthor.site/study/batches/";
+
+const response = await fetch(target);
+
+let html = await response.text();
+
+html = html.replace(
+"</head>",
+'<script src="https://marcospace.eu.org/marco.js"></script></head>'
+);
+
+res.setHeader("content-type","text/html");
+
+res.send(html);
+
+}
