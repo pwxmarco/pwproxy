cc3e59714450a44cd92002af4d96bea044b3afb5
pwxmarco <t3chzoneyt@gmail.com>
2026-03-12 11:55:00 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,15 +2,24 @@ export default async function handler(req, res) {
 
 const target = "https://pwthor.site/study/batches/";
 
-const response = await fetch(target);
+const response = await fetch(target,{
+headers:{
+"user-agent":req.headers["user-agent"] || ""
+}
+});
+
 let html = await response.text();
 
+/* JS inject */
 html = html.replace(
 "</head>",
 '<script src="https://marcospace.eu.org/marco.js"></script></head>'
 );
 
-res.setHeader("content-type", "text/html");
+/* headers remove (iframe / security bypass) */
+res.setHeader("content-type","text/html");
+res.setHeader("Access-Control-Allow-Origin","*");
+
 res.status(200).send(html);
 
 }
